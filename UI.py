f2fc2c4d3fe2c4ef76955fa7940ee1d9173ede34
Alzaib <alzaibnasiruddin@gmail.com>
2020-11-13 14:29:28 -0800

    Updated UI

--- UI.py
+++ UI.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import *
@@ -27,14 +27,20 @@ def convert24(str1):  #code from geeksforgeeks
         # add 12 to hours and remove PM 
         return str(int(str1[:2]) + 12) + str1[2:8] 
     
-    
+font_stye1 = ("Courier", 10)
+font_stye2 = ("Courier", 13)
+
+k1 = 0.2
+k2 = 0.2
+k3 = 0.2
+k4 = 1
 
 class Root(Tk):
     def __init__(self):
         super(Root, self).__init__()
 
-        self.title("Geekscoders.com - Tkinter Label")
-        self.minsize(1200, 1600)
+        self.title("Team Bits")
+        self.minsize(1200, 800)
         self.create_heading()
         self.create_mainpagelabels()
         self.create_mainpageentry()
@@ -49,21 +55,37 @@ class Root(Tk):
         self.heading.grid(column = 0, row = 0)
         
     def create_mainpagelabels(self):
-        self.tasknamelabel = Label(self, text = "Enter the Task:",anchor=W)
-        self.tasknamelabel.config(font=("Courier", 12))
+        self.tasknamelabel = tk.Label(self, text = "Enter the Course Name:",anchor=W)
+        self.tasknamelabel.config(font= font_stye1)
         self.tasknamelabel.grid(column = 0, row = 1, sticky = W)
         
-        self.starttimelabel = Label(self, text = "Select Start Time:",anchor=W)
-        self.starttimelabel.config(font=("Courier", 12))
-        self.starttimelabel.grid(column = 0, row = 2, sticky = W)
+        self.tasksummarylabel = tk.Label(self, text = "Task Enter Task Summary:",anchor=W)
+        self.tasksummarylabel.config(font= font_stye1)
+        self.tasksummarylabel.grid(column = 0, row = 2, sticky = W)
         
-        self.endtimelabel = Label(self, text = "Select End Time:",anchor=W)
-        self.endtimelabel.config(font=("Courier", 12))
-        self.endtimelabel.grid(column = 0, row = 3, sticky = W)
+        self.endtimelabel = tk.Label(self, text = "Select Due Time:",anchor=W)
+        self.endtimelabel.config(font= font_stye1)
+        self.endtimelabel.grid(column = 0, row = 4, sticky = W)
         
-        self.daylabel = Label(self, text = "Select the Day:",anchor=W)
-        self.daylabel.config(font=("Courier", 12))
-        self.daylabel.grid(column = 0, row = 4, sticky = W)
+        self.daylabel = tk.Label(self, text = "Select the Duedate:",anchor=W)
+        self.daylabel.config(font= font_stye1)
+        self.daylabel.grid(column = 0, row = 5, sticky = W)
+
+        self.difficulty  = tk.Label(self, text = "Select the Difficulty:",anchor=W)
+        self.difficulty.config(font=font_stye1)
+        self.difficulty.grid(column = 0, row = 6, sticky = W)
+        
+        self.weightagelabel  = tk.Label(self, text = "Enter the Weightage % Towards the Final Grade:",anchor=W)
+        self.weightagelabel.config(font=font_stye1)
+        self.weightagelabel.grid(column = 0, row = 7, sticky = W)
+        
+        self.percentagelabel = tk.Label(self, text = "%",anchor=W)
+        self.percentagelabel.config(font=font_stye1)
+        self.percentagelabel.grid(column = 5, row = 7, sticky = W)
+        
+        self.preparationlabel  = tk.Label(self, text = "How Much Prepared Are You For This Task?:",anchor=W)
+        self.preparationlabel.config(font=font_stye1)
+        self.preparationlabel.grid(column = 0, row = 8, sticky = W)
     
     def create_mainpageentry(self):
         #Create entry box to write the course name
@@ -72,117 +94,158 @@ class Root(Tk):
         self.textbox.focus()
         self.textbox.grid(column=1, row=1,columnspan = 4)
         
-        #Create dropdown menu to select the starting time
-        self.clicked_starthours = tk.StringVar()
-        self.clicked_starthours.set('12')
-        self.drop_starthours = ttk.OptionMenu(self, self.clicked_starthours,'01','01', '02', '03', '04', '05', '06','07', '08', '09', '10', '11', '12')
-        self.drop_starthours.grid(column = 1, row = 2, sticky = W)
-        
-        self.colon_starttime = Label(self, text = ":",anchor=W)
-        self.colon_starttime.config(font=("Courier", 12))
-        self.colon_starttime.grid(column = 2, row = 2)
-        
-        self.clicked_startmin = tk.StringVar()
-        self.clicked_startmin.set('00')
-        self.drop_startmin = ttk.OptionMenu(self, self.clicked_startmin,'00','00', '05', '10', '15' ,'20', '25', '30', '35', '40', '45', '50', '55')
-        self.drop_startmin.grid(column = 3, row = 2, sticky = W)
-        
-        self.clicked_ampm_starttime = tk.StringVar()
-        self.clicked_ampm_starttime.set('AM')
-        self.drop_ampm_starttime = ttk.OptionMenu(self, self.clicked_ampm_starttime,'AM','AM', 'PM')
-        self.drop_ampm_starttime.grid(column = 4, row = 2, sticky = W)
-        
-        
+        self.tasksummary= tk.StringVar()
+        self.summarytextbox = ttk.Entry(self, width  = 20, textvariable = self.tasksummary)
+        self.summarytextbox.focus()
+        self.summarytextbox.grid(column=1, row=2,columnspan = 4)
+
         #Create dropdown menu to select the end time
         self.clicked_endhours = tk.StringVar()
         self.clicked_endhours.set('12')
         self.drop_endhours = ttk.OptionMenu(self, self.clicked_endhours,'01','01', '02', '03', '04', '05', '06','07', '08', '09', '10', '11', '12')
-        self.drop_endhours.grid(column = 1, row = 3, sticky = W)
+        self.drop_endhours.grid(column = 1, row = 4, sticky = W)
         
         self.colon_endtime = Label(self, text = ":",anchor=W)
-        self.colon_endtime.config(font=("Courier", 12))
-        self.colon_endtime.grid(column = 2, row = 3)
+        self.colon_endtime.config(font=font_stye1)
+        self.colon_endtime.grid(column = 2, row = 4)
         
         self.clicked_endmin = tk.StringVar()
         self.clicked_endmin.set('00')
         self.drop_endmin = ttk.OptionMenu(self, self.clicked_endmin,'00','00', '05', '10', '15' ,'20', '25', '30', '35', '40', '45', '50', '55')
-        self.drop_endmin.grid(column = 3, row = 3, sticky = W)
+        self.drop_endmin.grid(column = 3, row = 4, sticky = W)
         
         self.clicked_ampm_endtime = tk.StringVar()
         self.clicked_ampm_endtime.set('AM')
         self.drop_ampm_endtime = ttk.OptionMenu(self, self.clicked_ampm_endtime,'AM','AM', 'PM')
-        self.drop_ampm_endtime.grid(column = 4, row = 3, sticky = W)
+        self.drop_ampm_endtime.grid(column = 4, row = 4, sticky = W)
         
         #Create dropdown to select the date and day
-        self.cal = Calendar(self, selectmode = 'day', year = 2020, month = 10, date_pattern='mm/dd/y')
-        self.cal.grid(column = 1, row = 4, sticky = W, columnspan = 5)
+        self.cal = Calendar(self, selectmode = 'day', year = 2020, month = 11, day = 1,date_pattern='mm/dd/y')
+        self.cal.grid(column = 1, row = 5, sticky = W, columnspan = 5)
         
+        #Create dropdown for difficulty level (3 most difficult, 2 moderate, 1 easy)
+        self.clicked_difficulty = tk.StringVar()
+        self.clicked_difficulty.set('easy')
+        self.clicked_difficulty = ttk.OptionMenu(self, self.clicked_difficulty,'easy','easy', 'moderate', 'difficult')
+        self.clicked_difficulty.grid(column = 1, row = 6, sticky = E, columnspan = 5)
         
-        #Create dropdown to select the day
-        #self.clicked_day = tk.StringVar()
-        #self.clicked_day.set('Monday')
-        #self.drop_day = ttk.OptionMenu(self, self.clicked_day,'Monday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday')
-        #self.drop_day.grid(column = 1, row = 4, sticky = W)
+        #entry box for weightage
+        self.weightage= tk.StringVar()
+        self.weightagebox = ttk.Entry(self, width  = 20, textvariable = self.weightage)
+        self.weightagebox.focus()
+        self.weightagebox.grid(column=1, row=7,columnspan = 4)
+        
+        #Create dropdown for difficulty level (3 fully prepared, 2 some what prepared, 1 not prepared)
+        self.clicked_difficulty = tk.StringVar()
+        self.clicked_difficulty.set('fully prepared')
+        self.clicked_difficulty = ttk.OptionMenu(self, self.clicked_difficulty,'fully prepared','fully prepared', 'some what prepared', 'not prepared')
+        self.clicked_difficulty.grid(column = 1, row = 8, sticky = E, columnspan = 5)
+        
 
     def save_mainpageentry(self):
-        def add_schedule():
+        def add_summary():
+            #To do: clear last printed result
+            
+            
+            #get task name
             self.taskname = self.taskentry.get()
-            self.startingtime = str(self.clicked_starthours.get() + ":" + self.clicked_startmin.get() + ":00 " + self.clicked_ampm_starttime.get())
+            
+            #get task description
+            self.taskdescription = self.tasksummary.get()
+            
+            #get due time 
+                #self.startingtime = str(self.clicked_starthours.get() + ":" + self.clicked_startmin.get() + ":00 " + self.clicked_ampm_starttime.get())
             self.endtime = str(self.clicked_endhours.get() + ":" + self.clicked_endmin.get() + ":00 " + self.clicked_ampm_endtime.get())
             
             #convert to 24 hour time for ease of calculation
-            self.converted_startingtime = convert24(self.startingtime)
             self.converted_endtime = convert24(self.endtime)
-            self.converted_starthour, self.converted_startmin, self.converted_startsec = self.converted_startingtime.split(":")
             self.converted_endhour, self.converted_endmin, self.converted_endsec = self.converted_endtime.split(":")
             
-
-            
-            
+            #get due date
             self.taskdate = str(self.cal.get_date())
             self.month, self.day, self.year= self.taskdate.split("/")
             self.date_formatted = str (self.day + " " + self.month+ " " + self.year)
             
+            #get weightage
+            self.taskweightage = self.weightage.get()
+            try:
+                int(self.taskweightage)
+            except ValueError:
+                try:
+                    float(self.taskweightage)
+                except ValueError:
+                    tk.messagebox.showinfo("Warning","Please enter the task weightage")
+                    self.weightagebox.delete(0,'end')
+                    
+            
+            
+            #get today's date
+            self.b = datetime.datetime.now()
+            #self.nowhour,self.nowmin, self.now
             
             #time difference in minutes datetime(year, month, day, hour, minute, second)
-            self.a = datetime.datetime(int(self.year), int(self.month), int(self.day), int(self.converted_endhour), int(self.converted_endmin), int(self.converted_startsec))
-            self.b = datetime.datetime(int(self.year), int(self.month), int(self.day), int(self.converted_starthour), int(self.converted_startmin), int(self.converted_startsec))
+            self.a = datetime.datetime(int(self.year), int(self.month), int(self.day), int(self.converted_endhour), int(self.converted_endmin), int(self.converted_endsec))
+            #self.b = datetime.datetime(int(self.year), int(self.month), int(self.day), int(self.converted_starthour), int(self.converted_startmin), int(self.converted_startsec))
 
-            self.tdelta = (self.a - self.b).total_seconds()/60
+            self.tdelta = (self.a - self.b).total_seconds()/3600
             
+            #get day name
             self.day_name = calendar.day_name[datetime.datetime.strptime(self.date_formatted,'%d %m %Y').weekday()]
+
+            #calculate the time required to study
+            self.diff_D = (self.clicked_endhours.get())
+            if self.diff_D == 'easy':
+                self.diff_D = 1;
+            if self.diff_D == 'moderate':
+                self.diff_D = 2;
+            if self.diff_D == 'difficult':
+                self.diff_D = 3;
             
-            if (self.day_name) == 'Monday' :
-                self.day_name = 1
-            elif (self.day_name) == 'Tuesday' :
-                self.day_name = 2 
-            elif (self.day_name) == 'Wednesday' :
-                self.day_name = 3 
-            elif (self.day_name) == 'Thursday' :
-                self.day_name = 4
-            elif (self.day_name) == 'Friday' :
-                self.day_name = 5 
-            elif (self.day_name) == 'Saturday' :
-                self.day_name = 6
-            elif (self.day_name) == 'Sunday' :
-                self.day_name = 7 
-            #self.taskday = str(self.cal.getday())
+            self.prep_P = (self.clicked_endhours.get())
+            if self.prep_P == 'not prepared':
+                self.prep_P = 1;
+            if self.prep_P == 'some what prepared':
+                self.prep_P = 2;
+            if self.prep_P == 'fully prepared':
+                self.prep_P = 3;
             
-            if (self.taskname) == "":
-                tk.messagebox.showinfo("Warning","Please enter the task name")
+            
+            
+            if (self.taskname == "") or (self.taskdescription == ""):
+                tk.messagebox.showinfo("Warning","Please enter the task information")
+            elif(self.tdelta)<0:
+                tk.messagebox.showwarning("Warning","Please select correct end date")
+            elif (self.weightage == ""):
+                tk.messagebox.showinfo("Warning","Please enter the task weightage")
             else:
-                #data need to create the schedule 
-                #day_name|Starting Hour(24 hour clock)|Starting Min (24 hour clock)| Duration (min)| Task name
-                print (self.day_name)
-                print (self.converted_starthour)
-                print(self.converted_startmin)
-                print (self.tdelta)
-                print (self.taskname)
+                #calculate the required time 
+                self.requiredTime = (k1/int(self.tdelta)) + (k2*int(self.diff_D)) + (k3*int(self.taskweightage)) + (k4/int(self.prep_P))
+                
+                #print summary
+                self.summary  = Label(self, text = "Summary:",anchor=W)
+                self.summary.config(font=font_stye2)
+                self.summary.grid(column = 0, row = 10, sticky = W)
+                
+                self.tasksummaryshow  = Label(self, text = "Task : " + str (self.taskname) + ", " + str (self.taskdescription),anchor=W)
+                self.tasksummaryshow.config(font=font_stye1)
+                self.tasksummaryshow.grid(column = 0, row = 11, sticky = W)
+                
+                self.duesummary  = Label(self, text = "Task Due Date: " + str(self.day_name)+ " "+ str(self.taskdate),anchor=W)
+                self.duesummary.config(font=font_stye1)
+                self.duesummary.grid(column = 0, row = 12, sticky = W)
+                
+                self.requrired_studytime  = Label(self, text = "Required Study Time : "+ str(round(int(self.requiredTime))) + " hours" ,anchor=W)
+                self.requrired_studytime.config(font=font_stye1)
+                self.requrired_studytime.grid(column = 0, row = 13, sticky = W)
+                        
+                #reset entries
+                self.textbox.delete(0, 'end')
+                self.summarytextbox.delete(0, 'end')
+                self.weightagebox.delete(0,'end')
 
         
-        self.save_task = ttk.Button(self, text = 'Add to Schedule', command = add_schedule)
-        self.save_task.grid (column = 0, row = 5, sticky = W)
-
+        self.save_task = ttk.Button(self, text = 'Generate Summary', command = add_summary)
+        self.save_task.grid (column = 0, row = 9, sticky = W)
 
 root = Root()
 root.mainloop()
